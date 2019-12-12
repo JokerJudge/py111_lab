297247550983b4133e5ba43dea4ea72f34249263
Polytech <kovalenko_evgeny@bk.ru>
2019-12-12 21:20:44 +0300

    Попытка № 4 реализации задачи a0_my_stack

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -35,21 +35,24 @@ def pop() -> Any:
 
 
 def peek(ind: int = 0) -> Any:
-	"""
-	Allow you to see at the element in the stack without popping it.
+    """
+    Allow you to see at the element in the stack without popping it.
 
-	:param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
-	:return: peeked element or None if no element in this place
-	"""
-	#print(ind)
-	global stack
-	#print(stack)
-	if stack:
-		x = stack[len(stack) - 1 - ind]
-		print(x)
-		return x
-	else:
-		return None
+    :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
+    :return: peeked element or None if no element in this place
+    """
+    #print(ind)
+
+    global stack
+    #print(stack)
+    if stack:
+        try:
+            x = stack[len(stack) - 1 - ind]
+            return x
+        except:
+            return None
+    else:
+        return None
 
 
 def clear() -> None:
@@ -63,10 +66,11 @@ def clear() -> None:
 	return None
 
 if __name__ == "__main__":
-	push(1)
-	push(24)
-	push(47)
-	pop()
-	peek(0)
-	peek(1)
-	print(stack)
+    push(1)
+    push(24)
+    push(47)
+    print(stack)
+    pop()
+    peek(0)
+    peek(1)
+    print(stack)
