5d87e43d747900d4e34e70601077898c83cea8ae
JokerJudge <kovalenko_evgeny@bk.ru>
2019-12-15 14:11:37 +0300

    a2_priority_queue is done

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -5,6 +5,7 @@ Queue priorities are from 0 to 5
 """
 from typing import Any
 
+queue = []
 
 def enqueue(elem: Any, priority: int = 0) -> None:
 	"""
@@ -13,6 +14,8 @@ def enqueue(elem: Any, priority: int = 0) -> None:
 	:param elem: element to be added
 	:return: Nothing
 	"""
+	global queue
+	queue.append((priority, elem))
 	return None
 
 
@@ -22,7 +25,19 @@ def dequeue() -> Any:
 
 	:return: dequeued element
 	"""
-	return None
+	global queue
+	min = 5
+	if queue:
+		for i in queue: # находим сначала минимальный приоритет
+			if i[0] < min:
+				min = i[0]
+		for i in queue: # выбираем первый в списке элемент с минимальной очередью
+			if i[0] == min:
+				temp = i[1]
+				queue.remove(i)
+				return temp
+	else:
+		return None
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
@@ -32,7 +47,14 @@ def peek(ind: int = 0, priority: int = 0) -> Any:
 	:param ind: index of element (count from the beginning)
 	:return: peeked element
 	"""
-	return None
+	global queue
+	if queue:
+		try:
+			return queue[ind][1]
+		except IndexError:
+			return None
+	else:
+		return None
 
 
 def clear() -> None:
@@ -41,4 +63,9 @@ def clear() -> None:
 
 	:return: None
 	"""
+	global queue
+	queue = []
 	return None
+
+if __name__ == "__main__":
+	pass
