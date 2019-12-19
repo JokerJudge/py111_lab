bbeb083255722053bb5eaccfcadbe8cb841e02c1
Polytech <kovalenko_evgeny@bk.ru>
2019-12-19 18:51:41 +0300

    Линейный поиск (на паре)

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -2,6 +2,7 @@
 This module implements some functions based on linear search algo
 """
 from typing import Sequence
+import numpy as np
 
 
 def min_search(arr: Sequence) -> int:
@@ -13,3 +14,21 @@ def min_search(arr: Sequence) -> int:
 	"""
 	print(arr)
 	return -1
+
+def finding_elem(find_elem, array):
+	index = None
+	for i, elem in enumerate(array):
+		if elem == find_elem:
+			index = i
+			return index
+	#if find_elem in array:
+	#	return array.index(find_elem)
+
+	return index
+
+if __name__ == "__main__":
+	n = 1000
+	array = np.arange(n)
+	find_elem = np.random.choice(array)
+	np.random.shuffle(array)
+	print(finding_elem(find_elem, array))
