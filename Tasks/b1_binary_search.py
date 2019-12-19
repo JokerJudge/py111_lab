54994d9d43df2de31fea7dab831a37e49dbc348e
Polytech <kovalenko_evgeny@bk.ru>
2019-12-19 19:56:52 +0300

    b1_binary_search is done

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,14 +9,32 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
+	arr = sorted(arr)
 	print(elem, arr)
-    left = 0
-    right = len(arr) - 1
-    while True:
+	left = 0
+	right = len(arr) - 1
+	if elem in arr:
+		while True:
+			if right - left == 1:
+				if elem == arr[right]:
+					return right
+				elif elem == arr[left]:
+					return left
+				else:
+					return None
+			middle = (left + right) // 2
+			if elem == arr[middle]:
+				return middle
+			else:
+				if elem < arr[middle]:
+					right = middle - 1
+				elif elem > arr[middle]:
+					left = middle + 1
+
+	else:
+		return None
 
 
-
-	return None
 
 
 if __name__ == "__main__":
@@ -24,4 +42,4 @@ if __name__ == "__main__":
 	array = np.arange(n)
 	some_elem = np.random.choice(array)
 	np.random.shuffle(array)
-	print(finding_elem(find_elem, array))
+	print(binary_search(some_elem, array))
